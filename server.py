e535b37d519a98b17107e0b696a88dbb2e774b79
Mateo Usuga <13mateousuga@gmail.com>
2023-04-01 16:12:11 -0500

    first commit

---
+++ server.py
@@ -0,0 +1,33 @@
+from flask import Flask, render_template, request, redirect, session
+
+app = Flask(__name__)
+
+app.secret_key = 'dojoencuesta'
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/process', methods=['POST'])
+def process():
+    print(request.form)
+    
+    session['name'] = (request.form['name'])
+    session['location'] = request.form['location']
+    session['language'] = request.form['language']
+    session['comment'] = request.form['comment']
+    return redirect('/result')
+
+
+@app.route('/result')
+def result():
+    return render_template('/result.html')
+
+@app.route('/reset')
+def reset():
+    session.clear()
+    return redirect('/')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
