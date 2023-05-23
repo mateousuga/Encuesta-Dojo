8715ad8b0a7edbc54c50aad9387600152b9601d3
Mateo Usuga <13mateousuga@gmail.com>
2023-05-22 23:53:10 -0500

    change server

--- server.py
+++ server.py
@@ -1,30 +1,5 @@
-from flask import Flask, render_template, request, redirect, session
-
-
-
-@app.route('/')
-def index():
-    return render_template('index.html')
-
-@app.route('/process', methods=['POST'])
-def process():
-    print(request.form)
-    
-    session['name'] = request.form['name']
-    session['location'] = request.form['location']
-    session['language'] = request.form['language']
-    session['comment'] = request.form['comment']
-    return redirect('/result')
-
-
-@app.route('/result')
-def result():
-    return render_template('/result.html')
-
-@app.route('/reset')
-def reset():
-    session.clear()
-    return redirect('/')
+from flask_app import app
+from flask_app.controllers import dojos
 
 
 if __name__ == '__main__':
